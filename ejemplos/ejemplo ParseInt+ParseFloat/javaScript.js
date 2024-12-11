62b31f4ba09ae0a94ba0e625e588d97729d4257a
Najwaelqortobi <najwaelqortobi@gmail.com>
2024-12-11 12:35:00 +0100

    ejemplo String listo para generar codigo

---
+++ ejemplos/ejemplo ParseInt+ParseFloat/javaScript.js
@@ -0,0 +1,23 @@
+function calcularPrestamo() {
+    let monto = parseFloat(document.getElementById("loanAmount").value);
+    let tasa = parseFloat(document.getElementById("interestRate").value);
+    let plazo = parseInt(document.getElementById("loanTerm").value);
+    let caja = document.getElementById("resultado");
+    //si no es un number
+    if (isNaN(monto) || isNaN(tasa) || isNaN(plazo)) {
+        document.getElementById("resultado").innerHTML = "Por favor, ingrese valores numéricos válidos.";
+        return; 
+    }
+
+    let tasaMensual = tasa / 100 / 12;
+    let plazoPagos = plazo * 12;
+    let pagoMensual = (monto * tasaMensual * Math.pow(1 + tasaMensual, plazoPagos)) / (Math.pow(1 + tasaMensual, plazoPagos) - 1);
+
+    caja.innerHTML = `Pago mensual estimado: ${pagoMensual.toFixed(2)}€`;
+}
+
+/*Nota : parseFloat() se usa para convertir el monto del préstamo y la tasa de interés de strings a números decimales. Esto es crucial porque estos valores pueden contener decimales.
+parseInt() se utiliza para el plazo del préstamo, ya que esperamos un número entero de años.*/
+//Math.pow() en JavaScript para calcular una potencia.
+//toFixed(2): Redondear un número a una cantidad específica de decimales
+
---
+++ ejemplos/ejemplo String/javaScript.js
@@ -0,0 +1,24 @@
+function generarCodigoProducto() {
+    let categoriaProducto = document.getElementById("categoria").value;
+    let numeroProducto = parseInt(document.getElementById("numero").value);
+    let añoActual = new Date().getFullYear();
+
+    // Verificar si el número ingresado es válido
+    if (isNaN(numeroProducto)) {
+        document.getElementById("resultado").innerHTML = "Por favor, ingrese un número de producto válido.";
+        return;
+    }
+    let numeroProductoStr = String(numeroProducto).padStart(4, '0');
+    let codigoProducto = `${categoriaProducto}-${numeroProductoStr}`;
+
+    document.getElementById("resultado").innerHTML = `Código de producto generado: ${codigoProducto}-${añoActual}`;
+}
+
+
+
+/*Notas :
+//el primer paso seria convertir string a numeroInteger mediante parseInt()
+//el siguiente paso seria convertir nombre a String()
+//padStart(4, '0') agrega ceros al principio hasta que la longitud total de la cadena sea 4.
+//getFullYear(); esto devuelve la ultima parte de la fecha (Por ejemplo, si la fecha actual es 11 de diciembre de 2024, getFullYear() devolverá 2024)
+*/
