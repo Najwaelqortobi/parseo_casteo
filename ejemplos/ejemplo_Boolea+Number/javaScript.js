3a379c8c9fdf673b612297e382f7bc85adccd8cd
Najwaelqortobi <najwaelqortobi@gmail.com>
2024-12-11 11:14:12 +0100

    he puesto una carpeta ejemplos dentro ahora hay ejemplo Boolean+number por el caster/parseo

--- ejemplos/ejemplo_Boolea+Number/javaScript.js
+++ ejemplos/ejemplo_Boolea+Number/javaScript.js
@@ -4,6 +4,7 @@
             let respuestaJavascript = document.querySelector('input[name="javascript"]:checked');
 
             //la parte donde se convierte el Si Y el No de la encuesta en number y boolean
+            //respuestaProgramar && respuestaJavascript dentro del if sirve para verificar que el usuario ha respondido ambas preguntas de la encuesta antes de procesar los resultados. 
             if (respuestaProgramar && respuestaJavascript) {
                 let leGustaProgramar = Boolean(Number(respuestaProgramar.value));
                 let haUsadoJavascript = Boolean(Number(respuestaJavascript.value));
